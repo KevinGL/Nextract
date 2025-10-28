111f0702e0ee562e3dc34db9ddb5cd9760f18286
kevin <kevin.progc@gmail.com>
2025-10-28 20:02:04 +0100

    Erreur query engine prisma en prod (4)

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,11 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  generatorOverrides: {
+    client: {
+      binaryTargets: ["native", "rhel-openssl-3.0.x"],
+    },
+  },
   migrations: {
     path: "prisma/migrations",
   },
